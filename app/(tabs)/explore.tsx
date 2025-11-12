a63296c4cb43ceabde542a0a45af89213163e03f
Kevin Jay Herrera <kjayh@unal.edu.co>
2025-11-11 23:59:13 -0500

    gestion-de-gastos: creacion de la funcionalidad concerniente a la creacion, eliminacion, actulizacion y visualizacion de gastos

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,104 +1,83 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Label } from '@react-navigation/elements';
+import { useState } from 'react';
+import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput } from 'react-native';
 
-import { Collapsible } from '@/components/ui/collapsible';
-import { ExternalLink } from '@/components/external-link';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Fonts } from '@/constants/theme';
+interface Amount {
+  id: number,
+  value: number,
+}
 
 export default function TabTwoScreen() {
+  const [amount, setAmount] = useState<Amount[]>([]);
+  const [newValor, setNewValor] = useState<number | null>(0);
+  const [idAmount, setIdAmount] = useState<number | null>(null);
+  const [updatedValue, setUpdateValue] = useState<number>(0);
+
+  const addAmount = (newValue: number | null) => {
+    if (newValue === 0 || !newValue) return;
+
+    setAmount([...amount, { id: amount.length + 1, value: newValue }]);
+  }
+
+  const removeAmount = (id: number | null) => {
+    if (!id) return;
+
+    const newAmounts = amount.filter((e) => e.id !== id);
+    setAmount(newAmounts);
+  }
+
+  const updateAmount = (id: number | null, newValue: number) => {
+    if (!id || !newValue ) return;
+
+    const amountToUpdate = amount.find((e) => e.id === id);
+
+    if (amountToUpdate === undefined) return;
+
+    const newAmounts = amount.filter((e) => e.id !== id);
+    amountToUpdate.value = newValue;
+    newAmounts.push(amountToUpdate);
+
+    setAmount(newAmounts);
+  }
+
+  const totalAmount = () => {
+    let total = 0;
+
+    for (let index = 0; index < amount.length; index++) {
+      total += amount[index].value;
+    }
+    
+    return total;
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText
-          type="title"
-          style={{
-            fontFamily: Fonts.rounded,
-          }}>
-          Explore
-        </ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image
-          source={require('@/assets/images/react-logo.png')}
-          style={{ width: 100, height: 100, alignSelf: 'center' }}
-        />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user&apos;s current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful{' '}
-          <ThemedText type="defaultSemiBold" style={{ fontFamily: Fonts.mono }}>
-            react-native-reanimated
-          </ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+    <ScrollView style={styles.viewContainer}>
+      <Label style={styles.title}>Gestion de gastos</Label>
+      <FlatList data={amount.toSorted((a, b) => a.id - b.id)} renderItem={({ item }) => {
+        return (
+          <Text>{ item.value } <Button title='Remove' onPress={() => removeAmount(item.id)}/></Text>
+        )
+      }}>
+      </FlatList>
+
+      <TextInput keyboardType='numeric' placeholder='Ingresar monto' onChange={(e) => setNewValor(Number(e.nativeEvent.text))}/>
+      <Button title='Agregar' onPress={() => addAmount(newValor)} />
+
+
+      <TextInput keyboardType='numeric' placeholder='Id a actualizar' onChange={(e) => setIdAmount(Number(e.nativeEvent.text))}/>
+      <TextInput keyboardType='numeric' placeholder='Valor a actualizar' onChange={(e) => setUpdateValue(Number(e.nativeEvent.text))}/>
+
+      <Button title='Actualizar' onPress={() => updateAmount(idAmount, updatedValue)}/>
+
+        <Text>{totalAmount()}</Text>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  viewContainer: {
+    width: '100%',
+  },
   headerImage: {
     color: '#808080',
     bottom: -90,
@@ -106,7 +85,11 @@ const styles = StyleSheet.create({
     position: 'absolute',
   },
   titleContainer: {
+    marginTop: 500,
     flexDirection: 'row',
     gap: 8,
   },
+  title: {
+    marginTop: '10%',
+  }
 });
